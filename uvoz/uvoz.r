41133074436116a6942a2d76760d2c8ad58b57e2
lanaherman <45061277+lanaherman@users.noreply.github.com>
2019-10-21 17:01:35 +0200

    Update uvoz.r

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -79,6 +79,11 @@ library(mosaic)
 library(maptools)
 library(reshape2)
 library(readr)
+library(ggplot2)
+library(ggvis)
+library(ggmap)
+library(mapproj)
+library(munsell)
 
 Tabela1 <- read.csv2("podatki/Kmetijska gospodarstva.csv", na=c("", " ", "..."))
 colnames(Tabela1)=c("Kmetijska gospodarstva", "2000", "2003", "2005", "2007", "2010", "2013", "2016")
@@ -111,12 +116,10 @@ colnames(Tabela6)=c("Kmetijska gospodarstva po statisticnih regijah", "2003", "2
 Tabela6 <- Tabela6[2:13,]
 Tabela6 <- Tabela6 %>% melt(id.vars="Kmetijska gospodarstva po statisticnih regijah", variable.name="leto", value.name="stevilo kmetijskih gospodarstev")
 
+########################## ZEMLJEVID ############################################################
 source("https://raw.githubusercontent.com/jaanos/APPR-2018-19/master/lib/uvozi.zemljevid.r")
-
-
 #kraji <- read.csv2("uvoz/SVN_adm2a.csv", na=c("", " ", "..."), sep=",",encoding="UTF-8")
 obcine <- uvozi.obcine()
-
 
 b <- Tabela6[Tabela6$leto==2010, ]
 colnames(b) <- c("regija", "leto", "stevilo")
@@ -150,6 +153,92 @@ names(data)[names(data) == "stevilo"] <- "stevilo kmetijskih gospodarstev"
 #ggplot() + geom_polygon(data=data, aes(x=long, y=lat, group=group, fill="id")) +
 #  guides(fill=FALSE)
 
- p <- ggplot(data=data, aes(x=long, y=lat)) +
-   geom_polygon(aes(fill = `stevilo kmetijskih gospodarstev`, group = OB_IME))
- p
+p <- ggplot() + geom_polygon(data=left_join(zemljevid, total, by=c("OB_IME"="OB_IME")),
+                        aes(x=long, y=lat, group=group, fill=stevilo))
+p
+p + labs(fill = "stevilo kmetijskih gospodarstev v letu 2010")
+
+ #p <- ggplot(data=data, aes(x=long, y=lat, group=group)) +
+  # geom_polygon(aes(fill = `stevilo kmetijskih gospodarstev`, group = OB_IME))
+# p
+###############################################################################################
+
+########## HISTOGRAM RASTLINSKIH PRIDELKOV ####################################################
+c <- Tabela2[Tabela2$leto %in% c(2013, 2016), ]
+c[is.na(c)] <- 0
+d <- Tabela4[Tabela4$leto %in% c(2013, 2016), ]
+d[is.na(d)] <- 0
+
+Rastline <- aggregate(c$stevilo, by=list(c$leto), FUN=sum)
+Eko_rastline <- aggregate(d$stevilo, by=list(d$leto), FUN=sum)
+Rastline$vrsta="Rastlinski pridelki"
+Eko_rastline$vrsta="Ekoloski rastlinski pridelki"
+
+data_rastline <- merge(Rastline, Eko_rastline, all=TRUE)
+colnames(data_rastline)=c("leto", "stevilo", "vrsta")
+razmerje_2013 <- 100000*(data_rastline$stevilo[1]/data_rastline$stevilo[2])
+razmerje_2016 <- 100000*(data_rastline$stevilo[3]/data_rastline$stevilo[4])
+
+nov_data <- data.frame(leto=c("2013", "2016"), stevilo=c(razmerje_2013, razmerje_2016),
+                       vrsta=c("Razmerje pomnoženo z 100000"))
+nov_data_rastline <- rbind(data_rastline, nov_data)
+
+graf_rast <- ggplot(data = nov_data_rastline, aes(x=leto, y=stevilo, fill=vrsta)) +
+  geom_bar(stat="identity", position="dodge") +
+  scale_fill_manual("Legenda", values = c("Ekoloski rastlinski pridelki" = "orange4", "Rastlinski pridelki" = "green4", "Razmerje pomnoženo z 100000" = "gray13"))
+graf_rast + labs(title="Histogram rastlinskih pridelkov", 
+                x="Leto", y = "Stevilo rastlinskih pridelkov (vsota)")
+##############################################################################################
+
+####################### ČRTNI GRAFIKON KMETIJSKIH GOSPODARSTEV ###############################
+Tabela2[is.na(Tabela2)] <- 0
+rast_ne_eko <- aggregate(Tabela2$stevilo, by=list(Tabela2$leto), FUN=sum)
+rast_ne_eko$pridelek="Rastlinski pridelki"
+Tabela3[is.na(Tabela3)] <- 0
+ziv_ne_eko <- aggregate(Tabela3$stevilo, by=list(Tabela3$leto), FUN=sum)
+ziv_ne_eko$pridelek="Zivina"
+
+data_ne_eko <- merge(rast_ne_eko, ziv_ne_eko, all=TRUE)
+colnames(data_ne_eko)=c("leto", "stevilo", "pridelek")
+data_ne_eko$leto <- as.numeric(as.character(data_ne_eko$leto))
+
+graf_prid <- ggplot(data = data_ne_eko, aes(x=leto, y=stevilo, col=pridelek)) +
+  #geom_point() + 
+  geom_line(size=1.5) +
+  scale_color_manual("Vrsta pridelka", values=c("Rastlinski pridelki" = "green4", "Zivina" = "hotpink4"))
+graf_prid + labs(x = "Leto", y = "Stevilo", title = "Crtni grafikon kmetijskih pridelkov")
+
+#tukaj napiši, da je v sloveniji veliko več kmetij z živini. razlaga je tukaj
+#https://www.stat.si/statweb/News/Index/6742
+
+##############################################################################################
+
+############# ALI JE VEDNO VEČ EKOLOŠKIH? #####################
+library(plyr)
+library(dplyr)
+
+tab <- Tabela1[Tabela1$`Kmetijska gospodarstva` %in% c("Kmetijska gospodarstva", "Kmetijska gospodarstva z ekološkim kmetovanjem", "Kmetijska gospodarstva v postopku preusmeritve v ekološko kmetovanje"), ]
+tab$`Kmetijska gospodarstva`[tab$`Kmetijska gospodarstva` == "Kmetijska gospodarstva v postopku preusmeritve v ekološko kmetovanje"] <- "Kmetijska gospodarstva z ekološkim kmetovanjem"
+tab[is.na(tab)] <- 0
+#m <- cbind(t(apply(tab[1:2], 1, sort)), tab[,3])
+#aggregate(m[,3], by = list(m[,1],m[,2]),FUN=sum)
+#tab_new = tab %>% group_by(`Kmetijska gospodarstva`) %>% summarise(tab$`Kmetijska gospodarstva` = sum(stevilo))
+##############################################################################################
+
+library(ggplot2movies)
+require(ggplot2movies)
+
+head(movies)
+
+tipi = names(movies)[18:23]
+seznam = list()
+for (i in 1:length(tipi)) {
+  tip = tipi[[i]]
+  seznam[[i]] <- movies %>% 
+    filter_(paste(tip, "==", 1)) %>% 
+    select(Budget=budget, Short, Year=year) %>%
+    mutate(Type=tip)
+}
+myMovies <- do.call(rbind, seznam)
+
+ggplot(data=myMovies, aes(x=Year, y=Type, size=Budget)) + geom_point()
